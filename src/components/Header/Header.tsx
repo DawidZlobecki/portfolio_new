df7de5744950e612a55e6e6a8ecd74efd15ef442
d.zlobecki <d.zlobecki@mymusic.pl>
2024-09-02 18:24:54 +0200

    feat: profileImage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Header.scss";
+import ProfileImage from "../ProfileImage/ProfileImage";
 
-const Header = ()=> {
-    return (<header>header</header>)
-}
+const Header = () => {
+  return (
+    <header className="header">
+      <ProfileImage />
+    </header>
+  );
+};
 
-export default Header
+export default Header;
