1b05eb801f879af0cbacdb8dd332cedb95b84674
d.zlobecki <d.zlobecki@mymusic.pl>
2025-01-09 11:30:58 +0100

    feat: fixes

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -6,22 +6,23 @@ import {
   faLinkedin,
   faGitlab,
 } from "@fortawesome/free-brands-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 const navigationLinks = [
   {
     id: 1,
     link: "https://github.com/DawidZlobecki",
-    element: <FontAwesomeIcon icon={faGithub} className="icon" />,
+    element: <FontAwesomeIcon icon={faGithub as IconProp} className="icon" />,
   },
   {
     id: 2,
     link: "https://www.linkedin.com/in/dawid-zlobecki-3b4765215/",
-    element: <FontAwesomeIcon icon={faLinkedin} className="icon" />,
+    element: <FontAwesomeIcon icon={faLinkedin as IconProp} className="icon" />,
   },
   {
     id: 3,
     link: "https://gitlab.com/DawidZlobecki",
-    element: <FontAwesomeIcon icon={faGitlab} className="icon" />,
+    element: <FontAwesomeIcon icon={faGitlab as IconProp} className="icon" />,
   },
 ];
 const Navigation = () => {
